89a510432d67f8f2d3f727ea844e7de1aa64a370
Elangovan16 <pmeelangovanm@gmail.com>
2023-08-11 17:21:24 +0530

    Completed

--- Java_L2_Programs/src/Program_15.java
+++ Java_L2_Programs/src/Program_15.java
@@ -1,4 +1,4 @@
-/*15.Given an array of values persons[], each represents the weight of the persons. There will be infinite bik
+/*15.  Given an array of values persons[], each represents the weight of the persons. There will be infinite bik
 es available. Given a value K which represents the maximum weight that a bike accommodates. Along wit
 h that one more condition, a bike can carry two persons at a time. You need to find out the least number o
 f times, the bike trips are made.*/
